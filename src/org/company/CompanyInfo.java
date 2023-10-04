b5755cefef3176451ea3edbdb34c8e627af82066
varshkarthi <varshkarthikeyan@gmail.com>
2023-10-04 15:57:49 +0530

    QA Code

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -2,22 +2,27 @@ package org.company;
 
 public class CompanyInfo {
 
-private void companyName() {
-	System.out.println("Company Name : Greens Technology");
-}
-private void companyId() {
-	System.out.println("Company ID ; 2246");
-}
-private void companyAddress() {
-	System.out.println("Company Address : 51A,"
-			+ "2nd floor,"
-			+ "Velachery,"
-			+ "Chennai-42");
-}
-public static void main(String[]args) {
-	CompanyInfo c = new CompanyInfo();
-	c.companyName();
-	c.companyId();
-	c.companyAddress();
-}
+	private void companyName() {
+		System.out.println("Company Name : Greens Technology");
+	}
+
+	private void companyId() {
+		System.out.println("Company ID ; 2246");
+	}
+
+	private void companyAddress() {
+		System.out.println("Company Address : 51A," + "2nd floor," + "Velachery," + "Chennai-42");
+	}
+
+	private void companyLAN() {
+		System.out.println("Company LAN ; 044-0252246");
+	}
+
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+		c.companyLAN();
+	}
 }
